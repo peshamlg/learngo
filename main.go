a07c9e40e5455f6dca52733f7372916e3b57f0f0
Taehyeong (Walter) Kim <diddu9189@gmail.com>
2024-09-08 13:22:17 +0900

    Create main.go
    Create main.go

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import(
+	"fmt"
+	"accounts/accounts"
+)
+
+func main() {
+	account := accounts.NewAccount("walter")
+	fmt.Println(account)
+
+	errDeposit := account.Deposit(10)
+	if errDeposit != nil {
+		fmt.Println(errDeposit)
+	}
+	fmt.Println(account)
+
+	errWithdraw := account.Withdraw(20)
+	if errWithdraw != nil {
+		fmt.Println(errWithdraw)
+	}
+	fmt.Println(account)
+}
