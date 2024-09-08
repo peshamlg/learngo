312255298df363813cd923467b8654cae74f6b58
Taehyeong (Walter) Kim <diddu9189@gmail.com>
2024-09-08 14:54:02 +0900

    Create mydict.go for repl.it test

---
+++ mydict/mydict.go
@@ -0,0 +1,47 @@
+package mydict
+
+import "errors"
+
+type Dictonary map[string]string
+
+// Search a word
+func (d Dictonary) Search (word string) (string, error) {
+  def, exist := d[word]
+  if exist {
+    return def, nil
+  }
+  return "", errors.New("The word is not found")
+}
+
+// Add a word to the dictonary
+func (d Dictonary) Add(word, def string) error {
+  _, err := d.Search(word)
+  if err == nil {
+    d[word] = def
+  } else {
+    return errors.New("The word is already exist")
+  }
+  return nil
+}
+
+// Update a word
+func (d Dictonary) Update(word, def string) error {
+  _, err := d.Search(word)
+  if err != nil {
+    d[word] = def
+  } else {
+    return errors.New("The word is not exist")
+  }
+  return nil
+}
+
+// Delete a word
+func (d Dictonary) Delete (word string) error {
+  _, err := d.Search(word)
+  if err != nil {
+    delete(d, word)
+  } else {
+    return errors.New("The word is not exist")
+  }
+  return nil
+}
