6a3cff4fe8526e322b948c7ee7c2e045b2997dc8
Taehyeong (Walter) Kim <diddu9189@gmail.com>
2024-09-08 14:27:53 +0900

    Update main.go
    add usecase of methods (search, add, update, delete)

--- dictonaryexample/main.go
+++ dictonaryexample/main.go
@@ -7,4 +7,51 @@ import (
 
 func main() {
   dictionary := mydict.Dictonary{}
+
+  baseWord := "hello"
+  def, errSearch := dictionary.Search(baseWord)
+  if errSearch != nil {
+    fmt.Println(errSearch)
+  } else {
+    fmt.Println(def)
+  }
+  
+  errAdd := dictionary.Add(baseWord, "First Word")
+  if errAdd != nil {
+    fmt.Println(errAdd)
+  } else {
+    fmt.Println("The word is added to dictonary")
+    def, errSearch = dictionary.Search(baseWord)
+    if errSearch != nil {
+      fmt.Println(errSearch)
+    } else {
+      fmt.Println(def)
+    }
+  }
+
+  errUpdate := dictonary.Update(baseWord, "Second Word")
+  if errUpdate != nil {
+    fmt.Println(errUpdate)
+  } else {
+    fmt.Println("The definition of word is updated")
+    def, errSearch = dictionary.Search(baseWord)
+    if errSearch != nil {
+      fmt.Println(errSearch)
+    } else {
+      fmt.Println(def)
+    }
+  }
+
+  errDelete := dictonary.Delete(baseWord)
+  if errDeelete != nil {
+    fmt.Println(errDelete)
+  } else {
+    fmt.Println("The word is deleted")
+    def, errSearch = dictionary.Search(baseWord)
+    if errSearch != nil {
+      fmt.Println(errSearch)
+    } else {
+      fmt.Println(def)
+    }
+  }
 }
