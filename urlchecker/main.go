623947b1a51d98f16d6e352c2cf75e4358c19847
peshamlg <diddu9189@gmail.com>
2024-09-17 13:36:53 +0900

    use go routine for url checker

--- urlchecker/main.go
+++ urlchecker/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
 )
 
+type requestResult struct {
+	url    string
+	status string
+}
+
 func main() {
-	var results = map[string]string{}
+	results := make(map[string]string)
+	c := make(chan requestResult)
 	urls := []string{
 		"https://www.google.com",
 		"https://www.facebook.com",
@@ -16,25 +21,29 @@ func main() {
 		"https://www.linkedin.com",
 		"https://www.github.com",
 		"https://www.youtube.com",
+		"https://www.naver.com",
+		"https://www.daum.net",
+		"https://www.nate.com",
+		"https://www.kakao.com",
+		"https://www.line.com",
 	}
 	for _, url := range urls {
-		result := "Success"
-		err := hitURL(url)
-		if err != nil {
-			result = "Failed"
-		}
-		results[url] = result
+		go hitURL(url, c)
 	}
-	for url, result := range results {
-		fmt.Println(url, result)
+	for i := 0; i < len(urls); i++ {
+		result := <-c
+		results[result.url] = result.status
+	}
+	for url, status := range results {
+		fmt.Println(url, status)
 	}
 }
 
-func hitURL(url string) error {
-	fmt.Println("Checking:", url)
+func hitURL(url string, c chan<- requestResult) {
 	response, err := http.Get(url)
+	status := "Success"
 	if err != nil || response.StatusCode >= 400 {
-		return errors.New("request failed")
+		status = "Failed"
 	}
-	return nil
+	c <- requestResult{url: url, status: status}
 }
