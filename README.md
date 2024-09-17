9fe0ea595c2027ec168919dde52683dc9e4fff17
peshamlg <diddu9189@gmail.com>
2024-09-17 16:55:09 +0900

    make scrapper faster using Go Routine and Channel, and update README.md

--- README.md
+++ README.md
@@ -29,3 +29,10 @@ map을 이용하여 기본적인 사전 구조를 만드는 실습
 
 - 웹 페이지에서 채용 공고를 스크래핑하는 실습
   - 실제 강의에서는 indeed를 사용하였으나, 해당 페이지가 변경되어 saramin에서 진행
+- 비동기 처리를 위해 Go Routine과 Channel을 사용
+
+---
+
+### To Do
+
+- jobscrapper의 `writeJobs` 함수를 Go Routine을 이용하여 빠르게 동작하도록 하기
--- jobscrapper/main.go
+++ jobscrapper/main.go
@@ -26,18 +26,102 @@ type job struct {
 	badge     string
 }
 
-func main() {
-	totalJobs := []job{}
-	jobsCount := getJobsCount()
-	totalPages := (jobsCount / defaultPerPage) + 1
+func checkError(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
 
-	for i := 0; i < totalPages; i++ {
-		jobs := getPage(i + 1)
-		totalJobs = append(totalJobs, jobs...)
+func checkCode(response *http.Response) {
+	if response.StatusCode != 200 {
+		log.Fatalln("Request failed with Status:", response.StatusCode)
+	}
+}
+
+func cleanString(str string) string {
+	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
+}
+
+func getJobsCount() int {
+	jobsCount := 0
+	response, err := http.Get(baseURL)
+	checkError(err)
+	checkCode(response)
+
+	defer response.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(response.Body)
+	checkError(err)
+
+	pageJobsCount := doc.Find("div.header").Find("span.cnt_result").Text()
+	re := regexp.MustCompile(`\d+`)
+	numStr := strings.Join(re.FindAllString(pageJobsCount, -1), "")
+	count, err := strconv.Atoi(numStr)
+	checkError(err)
+	jobsCount += count
+
+	return jobsCount
+}
+
+func getPage(page int, mc chan<- []job) {
+	jobs := []job{}
+	c := make(chan job)
+	pageURL := baseURL + "&recruitPage=" + strconv.Itoa(page)
+	response, err := http.Get(pageURL)
+	checkError(err)
+	checkCode(response)
+
+	defer response.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(response.Body)
+	checkError(err)
+
+	searchResult := doc.Find("div.item_recruit")
+	searchResult.Each(func(i int, card *goquery.Selection) {
+		go extractJob(card, c)
+	})
+
+	for i := 0; i < searchResult.Length(); i++ {
+		job := <-c
+		jobs = append(jobs, job)
 	}
 
-	writeJobs(totalJobs)
-	fmt.Println("Done")
+	mc <- jobs
+}
+
+func extractJob(card *goquery.Selection, c chan<- job) {
+	id, _ := card.Attr("value")
+	id = cleanString(id)
+	title := cleanString(card.Find("div.area_job").Find("h2.job_tit").Find("a").Find("span").Text())
+	company := cleanString(card.Find("div.area_corp").Find("strong.corp_name").Text())
+	location := ""
+	condition := []string{}
+	conditionResult := card.Find("div.area_job").Find("div.job_condition").Find("span")
+	conditionResult.Each(func(i int, cd *goquery.Selection) {
+		conditionAtag := cd.Find("a")
+		if conditionAtag.Length() > 0 {
+			conditionAtag.Each(func(i int, conditionLocation *goquery.Selection) {
+				if location != "" {
+					location += " "
+				}
+				location += cleanString(conditionLocation.Text())
+			})
+		} else {
+			condition = append(condition, cleanString(cd.Text()))
+		}
+	})
+	jobDate := cleanString(card.Find("div.area_job").Find("div.job_date").Find("span.date").Text())
+	badge := cleanString(card.Find("div.area_badge").Find("span").Text())
+
+	c <- job{
+		id:        id,
+		title:     title,
+		company:   company,
+		location:  location,
+		condition: condition,
+		jobDate:   jobDate,
+		badge:     badge,
+	}
 }
 
 func writeJobs(jobs []job) {
@@ -66,95 +150,21 @@ func writeJobs(jobs []job) {
 	}
 }
 
-func extractJob(card *goquery.Selection) job {
-	id, _ := card.Attr("value")
-	id = cleanString(id)
-	title := cleanString(card.Find("div.area_job").Find("h2.job_tit").Find("a").Find("span").Text())
-	company := cleanString(card.Find("div.area_corp").Find("strong.corp_name").Text())
-	location := ""
-	condition := []string{}
-	conditionResult := card.Find("div.area_job").Find("div.job_condition").Find("span")
-	conditionResult.Each(func(i int, cd *goquery.Selection) {
-		conditionAtag := cd.Find("a")
-		if conditionAtag.Length() > 0 {
-			conditionAtag.Each(func(i int, conditionLocation *goquery.Selection) {
-				if location != "" {
-					location += " "
-				}
-				location += cleanString(conditionLocation.Text())
-			})
-		} else {
-			condition = append(condition, cleanString(cd.Text()))
-		}
-	})
-	jobDate := cleanString(card.Find("div.area_job").Find("div.job_date").Find("span.date").Text())
-	badge := cleanString(card.Find("div.area_badge").Find("span").Text())
+func main() {
+	totalJobs := []job{}
+	jobsCount := getJobsCount()
+	totalPages := (jobsCount / defaultPerPage) + 1
+	mc := make(chan []job)
 
-	return job{
-		id:        id,
-		title:     title,
-		company:   company,
-		location:  location,
-		condition: condition,
-		jobDate:   jobDate,
-		badge:     badge,
+	for i := 0; i < totalPages; i++ {
+		go getPage(i+1, mc)
 	}
-}
 
-func cleanString(str string) string {
-	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
-}
-
-func getPage(page int) []job {
-	jobs := []job{}
-	pageURL := baseURL + "&recruitPage=" + strconv.Itoa(page)
-	response, err := http.Get(pageURL)
-	checkError(err)
-	checkCode(response)
-
-	defer response.Body.Close()
-
-	doc, err := goquery.NewDocumentFromReader(response.Body)
-	checkError(err)
-
-	searchResult := doc.Find("div.item_recruit")
-	searchResult.Each(func(i int, card *goquery.Selection) {
-		job := extractJob(card)
-		jobs = append(jobs, job)
-	})
-
-	return jobs
-}
-
-func getJobsCount() int {
-	jobsCount := 0
-	response, err := http.Get(baseURL)
-	checkError(err)
-	checkCode(response)
-
-	defer response.Body.Close()
-
-	doc, err := goquery.NewDocumentFromReader(response.Body)
-	checkError(err)
-
-	pageJobsCount := doc.Find("div.header").Find("span.cnt_result").Text()
-	re := regexp.MustCompile(`\d+`)
-	numStr := strings.Join(re.FindAllString(pageJobsCount, -1), "")
-	count, err := strconv.Atoi(numStr)
-	checkError(err)
-	jobsCount += count
-
-	return jobsCount
-}
-
-func checkError(err error) {
-	if err != nil {
-		log.Fatalln(err)
+	for i := 0; i < totalPages; i++ {
+		jobs := <-mc
+		totalJobs = append(totalJobs, jobs...)
 	}
-}
 
-func checkCode(response *http.Response) {
-	if response.StatusCode != 200 {
-		log.Fatalln("Request failed with Status:", response.StatusCode)
-	}
+	writeJobs(totalJobs)
+	fmt.Println("Done! Total jobs:", len(totalJobs))
 }
